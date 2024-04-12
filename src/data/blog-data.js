cb4ed6cbf3de176247bc5ad108e305c6d55b991d
General-Mudkip <bencethegreat@gmail.com>
2024-04-12 23:09:14 +0100

    Various changes

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -4,78 +4,77 @@ import { getClient } from "../lib/graphQLClient";
 export const getAllPosts = async () => {
   const client = getClient();
 
-  const allPosts = await client.request(
-    gql`
-      query allPosts {
-        publication(host: "blog.mudkip.live") {
-          title
-          posts(first: 20) {
-            pageInfo{
-              hasNextPage
-              endCursor
-            }
-            edges {
-              node {
-                author{
-                  name
-                  profilePicture
-                }
-                title
-                subtitle
-                brief
-                slug
-                coverImage {
-                  url
-                }
-                tags {
-                  name
-                  slug
-                }
-                publishedAt
-                readTimeInMinutes
-              }
-            }
+  const allPosts = await client.request(gql`
+    query allPosts {
+      publication(host: "blog.mudkip.live") {
+        title
+        posts(first: 20) {
+          pageInfo {
+            hasNextPage
+            endCursor
           }
-        }
-      }
-    `
-  );
-
-  return allPosts;
-};
-
-export const getPost = async (slug) => {
-    const client = getClient();
-
-    const data = await client.request(
-      gql`
-        query postDetails($slug: String!) {
-          publication(host: "blog.greenroots.info") {
-            post(slug: $slug) {
-              author{
+          edges {
+            node {
+              author {
                 name
                 profilePicture
               }
-              publishedAt
               title
               subtitle
-              readTimeInMinutes
-              content{
-                html
+              brief
+              slug
+              coverImage {
+                url
               }
               tags {
                 name
                 slug
               }
-              coverImage {
-                url
-              }
+              publishedAt
+              readTimeInMinutes
             }
           }
         }
-      `,
-      { slug: slug }
-    );
+      }
+    }
+  `);
 
-    return data.publication.post;
-  };
+  return allPosts;
+};
+
+export const getPost = async (slug) => {
+  const client = getClient();
+
+  const data = await client.request(
+    gql`
+      query postDetails($slug: String!) {
+        publication(host: "blog.mudkip.live") {
+          post(slug: $slug) {
+            author {
+              name
+              profilePicture
+            }
+            publishedAt
+            title
+            subtitle
+            readTimeInMinutes
+            content {
+              html
+            }
+            tags {
+              name
+              slug
+            }
+            coverImage {
+              url
+            }
+          }
+        }
+      }
+    `,
+    { slug: slug },
+  );
+
+  return data.publication.post;
+};
+
